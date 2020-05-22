7e1595e9ed5548af6b5aad662cebbb4492c5a706
Maxim Alferov <malferov44@gmail.com>
2020-05-22 20:27:38 +0200

    python info

--- manifest/deploy.sh
+++ manifest/deploy.sh
@@ -1,14 +1,20 @@
 #/bin/bash
 set -e
-pip install jinja2-cli
 
-jinja2 frontend.yml \
-  -D registry=$registry \
-  -D actor=$actor \
-  -D app=$app \
-  -D service=$service \
-  -D ver=$ver |
-  kubectl apply -f -
+python --version
+python3 --version
+pip --version
+pip3 --version
+
+#pip install jinja2-cli
+
+#jinja2 frontend.yml \
+#  -D registry=$registry \
+#  -D actor=$actor \
+#  -D app=$app \
+#  -D service=$service \
+#  -D ver=$ver |
+#  kubectl apply -f -
 
 #kubectl apply -f service.yml
 #kubectl apply -f secret.yml
