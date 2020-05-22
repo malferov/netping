6a99fae6e810a7b57cfa6f2b1220484b8b21976c
Maxim Alferov <malferov44@gmail.com>
2020-05-22 20:10:53 +0200

    deploy init

--- manifest/deploy.sh
+++ manifest/deploy.sh
@@ -1,5 +1,12 @@
 #/bin/bash
-kubectl apply -f frontend.yml
-kubectl apply -f service.yml
-kubectl apply -f secret.yml
-kubectl apply -f ingress.yml
+#pip install jinja2-cli
+jinja2 frontend.yml \
+  -D registry=$registry \
+  -D actor=$actor \
+  -D app=$app \
+  -D service=$service \
+  -D ver=$ver |
+  kubectl apply -f -
+#kubectl apply -f service.yml
+#kubectl apply -f secret.yml
+#kubectl apply -f ingress.yml
