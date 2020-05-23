135dfacd432e5221860abdf93a386745fbc817b1
Maxim Alferov <malferov44@gmail.com>
2020-05-23 15:41:36 +0200

    no target

--- terraform/tls.sh
+++ terraform/tls.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-terraform apply -target=acme_certificate.cert -auto-approve
+terraform apply -auto-approve
 export crt=$(terraform output -json crt | jq '.value')
 export key=$(terraform output -json key | jq '.value')
