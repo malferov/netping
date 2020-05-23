0c022039c4c98d16b882caf0579329eb7637e938
Maxim Alferov <malferov44@gmail.com>
2020-05-23 12:43:25 +0200

    fix version debug

--- manifest/deploy.sh
+++ manifest/deploy.sh
@@ -9,9 +9,9 @@ jinja2 frontend.yml \
   -D actor=$actor \
   -D app=$app \
   -D service=$service \
-  -D ver=$ver |
-  kubectl apply -f -
+  -D ver=$ver
+#  kubectl apply -f -
 
-jinja2 secret.yml -D crt=$crt -D key=$key | kubectl apply -f -
-kubectl apply -f service.yml
-kubectl apply -f ingress.yml
+#jinja2 secret.yml -D crt=$crt -D key=$key | kubectl apply -f -
+#kubectl apply -f service.yml
+#kubectl apply -f ingress.yml
