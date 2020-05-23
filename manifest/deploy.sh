1fb8016d0a763e9ca045bad74a9c2ea5d9ff4776
Maxim Alferov <malferov44@gmail.com>
2020-05-23 18:47:14 +0200

    fix app name

--- manifest/deploy.sh
+++ manifest/deploy.sh
@@ -10,5 +10,5 @@ jinja2 secret.yml -D crt=$crt -D key=$key |
 jinja2 service.yml -D service=$service |
   kubectl apply -f -
 
-jinja2 ingress.yml -D service=$service -D domain=$domain |
+jinja2 ingress.yml -D service=$service -D app=$app -D domain=$domain |
   kubectl apply -f -
