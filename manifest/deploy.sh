f98bb48ac764f50de3be63c825020eb0f6f0ee05
Maxim Alferov <malferov44@gmail.com>
2020-05-23 13:35:18 +0200

    kubectl apply

--- manifest/deploy.sh
+++ manifest/deploy.sh
@@ -1,17 +1,12 @@
 #/bin/bash
 set -e
-
 sudo pip install jinja2-cli
-whereis jinja2
 
-jinja2 frontend.yml \
-  -D registry=$registry \
-  -D actor=$actor \
-  -D app=$app \
-  -D service=$service \
-  -D ver=$ver
-#  kubectl apply -f -
+jinja2 frontend.yml -D service=$service -D tag=$tag |
+  kubectl apply -f -
 
-#jinja2 secret.yml -D crt=$crt -D key=$key | kubectl apply -f -
-#kubectl apply -f service.yml
-#kubectl apply -f ingress.yml
+jinja2 secret.yml -D crt=$crt -D key=$key |
+  kubectl apply -f -
+
+kubectl apply -f service.yml
+kubectl apply -f ingress.yml
