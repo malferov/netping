84989fad02b19a3e6996dc3cafa0daa0fd53bb2f
Maxim Alferov <malferov44@gmail.com>
2020-05-27 02:06:18 +0200

    fix service

--- manifest/service.sh
+++ manifest/service.sh
@@ -6,8 +6,8 @@ if [ "$1" = "web" ]; then
   port=80
 fi
 
-jinja2 deployment.yml -D service=$service -D port=$port -D tag=$tag |
+jinja2 deployment.yml -D service=$1 -D port=$port -D tag=$tag |
   kubectl apply -f -
 
-jinja2 service.yml -D service=$service -D port=$port |
+jinja2 service.yml -D service=$1 -D port=$port |
   kubectl apply -f -
