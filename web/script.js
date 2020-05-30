b0e4cf5055e7247ea1e733a3db161780435f7f42
Maxim Alferov <malferov44@gmail.com>
2020-05-30 02:11:28 +0200

    format output

--- web/script.js
+++ web/script.js
@@ -9,7 +9,8 @@ const app = new Vue({
     main() {
       fetch(this.api + '/ping/v1/' + this.hostname)
         .then(response => response.json())
-        .then(data => this.output = data.message)
+        .then(data =>
+          this.output = data.message.replace(/(?:\r\n|\r|\n)/g, '<br>'))
         .catch(error => console.error(error))
     }
   }
