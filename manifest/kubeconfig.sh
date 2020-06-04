9aa5d4c0cf6c6ffc0a143730bd1a5ab57ab4b590
Maxim Alferov <malferov44@gmail.com>
2020-06-04 22:48:30 +0200

    kube config

--- manifest/kubeconfig.sh
+++ manifest/kubeconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-kubectl config set-cluster kube --server=$K8S_SERVER
+kubectl config set-cluster kube --server=https://$K8S_SERVER:6443
 kubectl config set clusters.kube.certificate-authority-data $K8S_CAD
 kubectl config set-context ci@kube --cluster=kube --user=ci --namespace=$app
 kubectl config use-context ci@kube
