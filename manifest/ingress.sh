effc8ee1d68599c10713f60c005ce61de31b260a
Maxim Alferov <malferov44@gmail.com>
2020-06-04 23:17:32 +0200

    render secret

--- manifest/ingress.sh
+++ manifest/ingress.sh
@@ -2,11 +2,11 @@
 set -e
 
 cd ../terraform
-jinja2 ../manifest/secret.yml -D crt=$(terraform output crt) -D key=$(terraform output key)
+jinja2 ../manifest/secret.yml -D crt=$(terraform output crt) -D key=$(terraform output key) \
   > ../manifest/secret.rendered
 
 cd ../manifest
 kubectl apply -f secret.rendered
 
-jinja2 ingress.yml -D app=$app -D domain=$domain
+jinja2 ingress.yml -D app=$app -D domain=$domain \
   | kubectl apply -f -
