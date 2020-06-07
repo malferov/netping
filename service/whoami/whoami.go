25772f9fee4e46f4c09c28969f249c12a140df7a
Maxim Alferov <malferov44@gmail.com>
2020-06-08 01:55:26 +0200

    whoami init

---
+++ service/whoami/whoami.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/glog"
+	"net/http"
+	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
+)
+
+var (
+	sha     = "unknown"
+	version = "dev"
+	date    = "unknown"
+	pod     string
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.Use(cors.Default())
+	r.GET("/", statusOk)
+	g := r.Group("/whoami")
+	{
+		g.GET("/version", getVersion)
+		g.GET("/ip", getIp)
+	}
+	return r
+}
+
+func main() {
+	var port string
+	flag.StringVar(&port, "port", "5000", "server listening port")
+	flag.Parse()
+
+	pod, _ = os.Hostname()
+
+	router := setupRouter()
+	router.Run(":" + port)
+}
+
+func getVersion(c *gin.Context) {
+	body := gin.H{
+		"sha":      sha,
+		"version":  version,
+		"date":     date,
+		"hostname": pod,
+		"ginmode":  gin.Mode(),
+		"lang":     "golang",
+	}
+	c.JSON(http.StatusOK, body)
+}
+
+func statusOk(c *gin.Context) {
+	c.Status(http.StatusOK)
+}
+
+func getIp(c *gin.Context) {
+	glog.Info("whoami: begin")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "0.0.0.0",
+	})
+	glog.Infof("whoami: %d", http.StatusOK)
+}
