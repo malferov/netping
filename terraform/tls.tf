8643f82f9a670c715e8293c2f6e342755f9b6c38
Maxim Alferov <malferov44@gmail.com>
2021-08-14 14:04:19 +0200

    cert update

--- terraform/tls.tf
+++ terraform/tls.tf
@@ -12,6 +12,7 @@ resource "acme_registration" "reg" {
   email_address   = var.email
 }
 
+# trigger A
 resource "acme_certificate" "cert" {
   account_key_pem           = acme_registration.reg.account_key_pem
   common_name               = "${var.app}.${var.domain}"
