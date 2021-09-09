c15a78e139242b18c9770718e801e6177e3a86ec
Maxim Alferov <malferov44@gmail.com>
2021-09-09 18:26:15 +0200

    debug

--- manifest/service.sh
+++ manifest/service.sh
@@ -6,6 +6,13 @@ if [ "$1" = "web" ]; then
   port=80
 fi
 
+len=${#tag}
+echo "debug tag=$tag len=$len"
+len=${#email}
+echo "debug email=$len"
+len=${#bot_token}
+echo "debug bot_token=$len"
+
 jinja2 deployment.yml -D service=$1 -D port=$port -D tag=$tag -D email=$email bot_token=$bot_token |
   kubectl apply -f -
 
