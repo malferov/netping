c02ccd18a88060e26b81b91f6d10daafcd0c5800
Maxim Alferov <malferov44@gmail.com>
2023-01-27 18:52:22 +0100

    fixup

---
+++ service/dns/dig.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+name=${1%.}
+
+for i in `seq 6`; do
+    SOA=`dig +noall +answer $name SOA | grep SOA | tr -s '[:space:]' ' ' | cut -d' ' -f5`
+    if [[ ${#SOA} -gt 0 ]]; then break; fi
+    name=${name#*.}
+    if [[ $name == ${name#*.} ]]; then break; fi
+done
+
+if [[ ${#SOA} -lt 4 ]];
+    then echo Not found; exit
+fi
+
+dig @$SOA +noall +answer $1 ANY
