aa399af0eaacc1327bc6e7b9e084bf0c60fea8c6
Maxim Alferov <malferov44@gmail.com>
2025-03-30 21:57:30 +0200

    cert update

--- infra/tls.tf
+++ infra/tls.tf
@@ -12,7 +12,7 @@ resource "acme_registration" "reg" {
   email_address   = var.email
 }
 
-# trigger G07
+# trigger G08
 resource "acme_certificate" "cert" {
   account_key_pem           = acme_registration.reg.account_key_pem
   common_name               = "${var.app}.${var.domain}"
