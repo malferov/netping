fd275a3ecbcaf57ca25be4735a877ebe2d66a934
Maxim Alferov <malferov44@gmail.com>
2025-07-29 09:27:46 +0200

    fix shell

--- service/dns/dig.sh
+++ service/dns/dig.sh
@@ -13,4 +13,4 @@ if [ ${#SOA} -lt 4 ];
     then echo Not found; exit
 fi
 
-dig @$SOA +noall +answer $1 ANY
+dig @$SOA +noall +answer +multiline $1 SOA $1 A $1 AAAA $1 TXT $1 MX $1 NS $1 PTR $1 CNAME $1 DNAME $1 CAA $1 CERT $1 HTTPS $1 DNSKEY $1 DS
